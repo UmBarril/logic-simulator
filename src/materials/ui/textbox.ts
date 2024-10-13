49fad2fc78a72d90dc4a58422f3c14b5ffe1663d
UmBarril <74427600+UmBarril@users.noreply.github.com>
2024-10-13 17:13:55 -0300

    adicionado TextBox (não tem rotações)

---
+++ src/materials/ui/textbox.ts
@@ -0,0 +1,42 @@
+import P5 from "p5"
+import { Material } from "../interfaces/material";
+import { Modifiers } from "../modifiers";
+
+export class TextBox extends Material {
+    private graphics: P5.Graphics;
+
+    constructor(
+        p: P5,
+        pos: P5.Vector,
+        private _title: string,
+        textSize = 40,
+        modifiers = new Modifiers<TextBox>()
+    ) {
+        super(pos, modifiers)
+        this.graphics = p.createGraphics(150,150);
+        this.graphics.clear()
+        this.graphics.textSize(textSize);
+    }
+
+    draw(p: P5): void {
+        const width = 100 // todo: remover esse tamanho e altura hardcoded
+        const height = 100
+        p.push()
+        p.translate(P5.Vector.add(this.pos, this.pointOfOrigin));
+
+        p.strokeWeight(0) // descomentar isso quando não estiver debugando
+        // this.graphics.rectMode(p.CENTER);
+        // this.graphics.textAlign(p.CENTER, p.CENTER);
+        this.graphics.text(this._title, 0, height / 2, width, height);
+        p.texture(this.graphics);
+        p.plane(width,height)
+
+        p.pop()
+    }
+
+    /** @todo */
+    isInside(_: P5.Vector): boolean {
+        return false;
+    }
+
+}
