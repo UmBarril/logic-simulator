ff1c404906dcd6b26b6edc66b72feed30103d2d6
UmBarril <74427600+UmBarril@users.noreply.github.com>
2024-10-17 18:51:06 -0300

    reorganização sem mudança na parte funcional do código

--- src/screens/ui.ts
+++ src/screens/ui.ts
@@ -3,9 +3,6 @@ import { MaterialGroup } from "../materials/interfaces/materialgroup";
 import { RotatingTitle } from "../materials/ui/rotatingtitle";
 
 export class UI extends MaterialGroup {
-    windowResized(p: P5) {
-        throw new Error("Method not implemented.");
-    }
 
     constructor(p: P5) {
         super(new P5.Vector(0, 0))
@@ -13,11 +10,18 @@ export class UI extends MaterialGroup {
         let title = 'logic simulator v0.1'
         let titleMaterial = new RotatingTitle(
             p, 
-            p.createVector(-p.width / 2 + 85, -p.height / 2 + 100), // posição
+            // -100 para ter certeza de que ele estará no fundo
+            p.createVector(-p.width / 2 + 85, -p.height / 2 + 100, -100), // posição
             title, // texto
             true // rainbowMode
         )
 
         this.addChild(titleMaterial)
     }
+
+    windowResized(p: P5) {
+        // throw new Error("Method not implemented.");
+        // TODO
+    }
+
 }
