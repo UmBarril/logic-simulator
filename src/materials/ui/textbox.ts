72282f0f0a5eb148263c702c0fac71bb6d25d5e1
UmBarril <74427600+UmBarril@users.noreply.github.com>
2024-10-22 02:41:30 -0300

    fazendo o textbox se mais usável

--- src/materials/ui/textbox.ts
+++ src/materials/ui/textbox.ts
@@ -4,33 +4,47 @@ import { Modifiers } from "../modifiers";
 
 // TODO: FAZER ISSO FUNCIONAR PROPRIAMENTE
 export class TextBox extends Material {
-    private graphics: P5.Graphics;
+    // private graphics: P5.Graphics;
+    private pi: P5.Element;
 
     constructor(
         p: P5,
         pos: P5.Vector,
         private _title: string,
-        pxTextSize = 40,  // em pixel
+        private pxTextSize = 40,  // em pixel
         modifiers = new Modifiers<TextBox>()
     ) {
         super(pos, modifiers)
-        this.graphics = p.createGraphics(150,150);
-        this.graphics.clear()
-        this.graphics.textSize(pxTextSize);
+        // this.graphics = p.createGraphics(150,150);
+        // this.graphics.clear()
+        // this.graphics.textSize(pxTextSize);
+        this.pi = p.createP(this._title)
+    }
+
+    solucaoComElement(p: P5){
+        this.pi.position(this.realPos.x + p.width / 2, this.realPos.y + p.height / 2)
+        this.pi.html(this._title)
+        this.pi.style("font-size", this.pxTextSize + "px")
     }
 
     draw(p: P5): void {
-        const width = 100 // todo: remover esse tamanho e altura hardcoded
-        const height = 100
         p.push()
-        p.translate(P5.Vector.add(this.pos, this.pointOfOrigin));
 
-        p.strokeWeight(0) // descomentar isso quando não estiver debugando
-        // this.graphics.rectMode(p.CENTER);
-        // this.graphics.textAlign(p.CENTER, p.CENTER);
-        this.graphics.text(this._title, 0, height / 2, width, height);
-        p.texture(this.graphics);
-        p.plane(width,height)
+        this.solucaoComElement(p)
+        
+        /////// (tentativa) de SOLUCAO COM GRAPHICS
+
+        // const width = 100 // todo: remover esse tamanho e altura hardcoded
+        // const height = 100
+
+        // p.translate(P5.Vector.add(this.pos, this.pointOfOrigin));
+
+        // // p.strokeWeight(0) // descomentar isso quando não estiver debugando
+        // // this.graphics.rectMode(p.CENTER);
+        // // this.graphics.textAlign(p.CENTER, p.CENTER);
+        // this.graphics.text(this._title, 0, height / 2, width, height);
+        // p.texture(this.graphics);
+        // p.plane(width,height)
 
         p.pop()
     }
